efe077c7f180ea7d42e2e895c4b7d7fe12e3f94f
Tar <pitupakorn@gmail.com>
2024-06-05 20:49:13 +0700

    Initial Commit

---
+++ primoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  primoApp
+//
+//  Created by Tar on 5/6/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
